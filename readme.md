ce91c9de326c5909c47b07bc0f4033a8772fc77d
paoloposso <pvictorsys@gmail.com>
2022-01-15 15:16:08 -0300

    add fourth list and update readme

--- readme.md
+++ readme.md
@@ -10,6 +10,8 @@ Box<T>: https://doc.rust-lang.org/book/ch15-01-box.html
 
 Rc<T>: https://doc.rust-lang.org/book/ch15-04-rc.html
 
+Arc<T>: https://doc.rust-lang.org/std/sync/struct.Arc.html
+
 ### Unit Testing in Rust
 
 https://doc.rust-lang.org/book/ch11-01-writing-tests.html
--- src/third_persistent.rs
+++ src/third_persistent.rs
@@ -1,10 +1,11 @@
-use std::rc::Rc;
+use std::sync::Arc;
+
 
 pub struct List<T> {
     pub head: Link<T>,
 }
 
-type Link<T> = Option<Rc<Node<T>>>;
+type Link<T> = Option<Arc<Node<T>>>;
 
 pub struct Node<T> {
     elem: T,
@@ -17,7 +18,7 @@ impl<T> List<T> {
     }
 
     pub fn prepend(&self, elem: T) -> List<T> {
-        List { head: Some(Rc::new(Node {
+        List { head: Some(Arc::new(Node {
             elem,
             next: self.head.clone(),
         }))}
@@ -29,6 +30,19 @@ impl<T> List<T> {
 
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem )
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Arc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
     }
 }
 
